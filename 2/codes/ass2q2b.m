2a03aa126e56f903cb847a78fdca1708856f984d
rahul-prajapat <rahulprajapat@cse.iitb.ac.in>
2021-02-22 09:25:14 +0530

    added codes for q2 and report

---
+++ 2/codes/ass2q2b.m
@@ -0,0 +1,66 @@
+clc; clear;
+
+I = imread("barbara256.png");
+J = double(I(1:256,1:256));
+figure;
+imshow(uint8(J));
+
+global A B alpha U;
+
+phi = randn(32,64);
+U = kron(dctmtx(8),dctmtx(8));
+A = phi*U;
+B = A.'*A;
+alpha = eigs(B,1)+2;
+
+[x, y] = size(J);
+
+R = zeros(x,y);
+D = zeros(x,y);
+%
+for i = 1:x-7
+    for j = 1:y-7
+        D(i:i+7,j:j+7) = D(i:i+7,j:j+7)+1;
+        R(i:i+7,j:j+7) = R(i:i+7,j:j+7)+ista(phi*reshape(J(i:i+7,j:j+7),64,1));
+    end
+end
+
+R = R./D;
+
+figure();
+imshow(uint8(R));
+
+function x = ista(y)
+    global A B alpha U
+    %disp(size(A.'));
+    %disp(size(y));
+    temp1 = A.'*y;
+    theta = randi(256,[64 1])-1;
+    i = 0;
+    while true
+        %thetanxt = soft(theta+(temp1-B*theta)/alpha,50/alpha);
+        %
+        theta = soft(theta+(temp1-B*theta)/alpha,50/alpha);
+        i = i+1;
+        if i>1000
+            break
+        end
+        %}
+        %{
+        if norm(thetanxt-theta) <= 0.1
+            theta = thetanxt;
+            break
+        end
+        theta = thetanxt;
+        %}
+    end
+    x = reshape(U*theta,8,8);
+end
+
+function x = soft(y,alpha)
+    x = zeros(size(y));
+    x( y >= alpha ) = y( y >= alpha )-alpha;
+    x( y <= -alpha ) = y( y <= -alpha )+alpha;
+end
+
+%}
