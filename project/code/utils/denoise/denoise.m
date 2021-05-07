2142671501c98a765bdfd8a0ea82d712056a8b5b
rahul-prajapat <rahulprajapat@cse.iitb.ac.in>
2021-05-07 23:17:39 +0530

    renamed sigma to omega

--- project/code/utils/denoise/denoise.m
+++ project/code/utils/denoise/denoise.m
@@ -1,17 +1,17 @@
 
-function Q=denoise(sigma, P)
-    %optimization : min (Q) 1/2||Q|sigma - P|sigma||^2_F + mu ||Q||_*
+function Q=denoise(omega, P)
+    %optimization : min (Q) 1/2||Q|omega - P|omega||^2_F + mu ||Q||_*
     %mu = (root(n1) + root(n2))*root(p)*variance
     [n1, n2] = size(P);
     Q=zeros(n1,n2);
     
-    p=sum(sigma,'all')/(n1*n2);
+    p=sum(omega,'all')/(n1*n2);
     
-    M=sigma.*P;
-    M=M-sum(M,2)./sum(sigma,2);
+    M=omega.*P;
+    M=M-sum(M,2)./sum(omega,2);
     M=M.^2;
-    M=sigma.*M;
-    v=sqrt(mean(sum(M,2)./sum(sigma,2)));
+    M=omega.*M;
+    v=sqrt(mean(sum(M,2)./sum(omega,2)));
     
     mu=(sqrt(n1)+sqrt(n2))*sqrt(p)*v;
     
@@ -19,7 +19,7 @@ function Q=denoise(sigma, P)
     epsilon=1e-5;
     
     for i=1:30
-        R = Q - tau*sigma.*(Q-P);
+        R = Q - tau*omega.*(Q-P);
         [U,D,V] = svd(R);
         D = max(D-tau*mu,0);
         Temp = U*D*V.';
