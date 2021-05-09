fbed94b1dfd97511cd878f99d79e82f481b28977
Sahasra Ranjan <sahasraranjan@gmail.com>
2021-05-09 16:09:23 +0530

    block matching done

---
+++ aip_project/code/costfnSAD.m
@@ -0,0 +1,13 @@
+% Ref:
+% New Technique of Three Step Search Algorithm used for Motion Estimation in Video Compression
+% Mr. Rahul Bhandari, Mr. Ashutosh Vyas
+
+
+function cost = costfnMAD(I1, I2, n)
+	err=0;
+	for i = 1:n
+		for j = 1:n
+			err = err + abs(I1(i,j) - I2(i,j))
+		end
+	end
+	cost = err;
